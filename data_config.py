c9a2bde90f15f2df624657354f2956be0b2465a4
maozan <758803455@qq.com>
2023-01-08 11:04:54 +0800

    Add files via upload

---
+++ data_config.py
@@ -0,0 +1,47 @@
+
+class DataConfig:
+    data_name = ""
+    root_dir = ""
+    label_transform = ""
+    def get_data_config(self, data_name):
+        self.data_name = data_name
+        if data_name == 'LEVIR':
+            self.label_transform = "norm"
+            self.root_dir = '/home/hwt/data/maozan/ChangeDetection/DataSets/LEVIR-CD-256/'
+        elif data_name == 'DSIFN':
+            self.label_transform = "norm"
+            self.root_dir = '/home/hwt/data/maozan/ChangeDetection/DataSets/DSIFN-CD-256/'
+        elif data_name == 'WHU':
+            self.label_transform = "norm"
+            self.root_dir = '/home/hwt/data/maozan/ChangeDetection/DataSets/WHU-CD-256/'
+        elif data_name == 'CDD':
+            self.label_transform = "norm"
+            self.root_dir = '/media/lidan/ssd2/CDData/CDD-CD-256/'
+        elif data_name == 'TYPO':
+            self.label_transform = "norm"
+            self.root_dir = '/media/lidan/ssd2/CDData/TYPO/'
+        elif data_name == 'quick_start_LEVIR':
+            self.root_dir = './samples_LEVIR/'
+        elif data_name == 'quick_start_DSIFN':
+            self.root_dir = './samples_DSIFN/'
+        elif data_name == 'quick_start_WHU':
+            self.root_dir = './samples_WHU/'
+        elif data_name == 'levir_sub_1':
+            self.root_dir = '/home/lcx/maozan/data/LEVIR-CD-256/'
+        elif data_name == 'levir_sub_2':
+            self.root_dir = '/home/lcx/maozan/data/LEVIR-CD-256/'
+        elif data_name == 'levir_sub_3':
+            self.root_dir = '/home/lcx/maozan/data/LEVIR-CD-256/'
+        elif data_name == 'levir_sub_4':
+            self.root_dir = '/home/lcx/maozan/data/LEVIR-CD-256/'
+        else:
+            raise TypeError('%s has not defined' % data_name)
+        return self
+
+
+if __name__ == '__main__':
+    data = DataConfig().get_data_config(data_name='LEVIR')
+    print(data.data_name)
+    print(data.root_dir)
+    print(data.label_transform)
+
