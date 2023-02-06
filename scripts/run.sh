94c37e1102a94317a36a1b57518f1b1cfebec435
maozan <758803455@qq.com>
2023-02-06 10:47:06 +0800

    add tag

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,12 +11,13 @@ dataset=CDDataset_edge  # CDDataset_edge
 
 
 img_size=256    
-batch_size=16   
+batch_size=8   
 lr=0.0001         
-max_epochs=100
+max_epochs=200
 embed_dim=256
+position_length=2
 
-net_G=HMNet_feature_lv_ca        # (HMNet_pure, no edge)、(HMNet_xx_lv, edge)
+net_G=HMNet_feature_lv_ca_fusion        # (HMNet_pure, no edge)、(HMNet_xx_lv, edge)
 
 lr_policy=linear
 optimizer=adamw                 #Choices: sgd (set lr to 0.01), adam, adamw
@@ -31,6 +32,6 @@ shuffle_AB=False
 #Train and Validation splits
 split=train         #trainval
 split_val=test      #test
-project_name=${net_G}_add_${dataset}_${data_name}_b${batch_size}_lr${lr}_${optimizer}_${split}_${split_val}_${max_epochs}_${lr_policy}_${loss}
+project_name=${net_G}_${dataset}_${data_name}_b${batch_size}_lr${lr}_${optimizer}_${split}_${split_val}_${max_epochs}_${lr_policy}_${loss}
 
-python main_cd.py --img_size ${img_size} --dataset ${dataset} --loss ${loss} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --optimizer ${optimizer} --split ${split} --split_val ${split_val} --net_G ${net_G} --multi_scale_train ${multi_scale_train} --multi_scale_infer ${multi_scale_infer} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --shuffle_AB ${shuffle_AB} --data_name ${data_name}  --lr ${lr} --embed_dim ${embed_dim}
+python main_cd.py --img_size ${img_size} --position_length ${position_length} --dataset ${dataset} --loss ${loss} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --optimizer ${optimizer} --split ${split} --split_val ${split_val} --net_G ${net_G} --multi_scale_train ${multi_scale_train} --multi_scale_infer ${multi_scale_infer} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --shuffle_AB ${shuffle_AB} --data_name ${data_name}  --lr ${lr} --embed_dim ${embed_dim}
